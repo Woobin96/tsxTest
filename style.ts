46040777e48375af36baf587f9b741b8cbca2058
wbkong <hmy1219@naver.com>
2025-04-07 16:13:11 +0900

    이것저것 시도해봄.
    - SPAQA- #####

---
+++ style.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'orange',
+        marginBottom: 24,
+    },
+    debugText: {
+        fontSize: 20,
+        color: 'red',
+    },
+});
+
